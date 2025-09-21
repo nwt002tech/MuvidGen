b5a38c4869df2e1933481d492ac1103d0800bb3c
nwt002tech <56565545+nwt002tech@users.noreply.github.com>
2025-09-20 22:12:06 -0500

    Update FontLoader.js

--- vendor/three/examples/jsm/loaders/FontLoader.js
+++ vendor/three/examples/jsm/loaders/FontLoader.js
@@ -1,1 +1,61 @@
+import {
+	Loader
+} from '../../../build/three.module.js';
 
+import { Font } from '../core/Font.js';
+
+/**
+ * https://github.com/mrdoob/three.js/blob/master/examples/jsm/loaders/FontLoader.js
+ */
+
+class FontLoader extends Loader {
+
+	constructor( manager ) {
+
+		super( manager );
+
+	}
+
+	load( url, onLoad, onProgress, onError ) {
+
+		const scope = this;
+
+		const loader = new this.constructor.DefaultFileLoader( this.manager );
+		loader.setPath( this.path );
+		loader.setRequestHeader( this.requestHeader );
+		loader.setWithCredentials( this.withCredentials );
+		loader.load( url, function ( text ) {
+
+			let json;
+
+			try {
+
+				json = JSON.parse( text );
+
+			} catch ( e ) {
+
+				if ( onError ) onError( e );
+
+				console.error( 'THREE.FontLoader: Can\'t parse ' + url + '.', e );
+
+				return;
+
+			}
+
+			const font = scope.parse( json );
+
+			if ( onLoad ) onLoad( font );
+
+		}, onProgress, onError );
+
+	}
+
+	parse( json ) {
+
+		return new Font( json );
+
+	}
+
+}
+
+export { FontLoader };
